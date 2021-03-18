6d87ecbcc08747b00201c9873c19d16a00776080
michael <michael@cadot.info>
2021-03-18 12:13:13 +0100

    feat:clean var and open page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,8 +13,47 @@ class HomeController extends AbstractController
      */
     public function index($page = 'index'): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/' . seo($page) . '.html.twig', [
             'page' => $page,
         ]);
     }
+}
+//source
+// https://stackoverflow.com/questions/14114411/remove-all-special-characters-from-a-string
+function seo($string)
+{
+    return strtolower(
+        preg_replace(
+            array('#[\\s-]+#', '#[^A-Za-z0-9. -]+#'),
+            array('-', ''),
+            cleanString(
+                urldecode($string)
+            )
+        )
+    );
+}
+
+function cleanString($text)
+{
+    $utf8 = array(
+        '/[áàâãªä]/u'   =>   'a',
+        '/[ÁÀÂÃÄ]/u'    =>   'A',
+        '/[ÍÌÎÏ]/u'     =>   'I',
+        '/[íìîï]/u'     =>   'i',
+        '/[éèêë]/u'     =>   'e',
+        '/[ÉÈÊË]/u'     =>   'E',
+        '/[óòôõºö]/u'   =>   'o',
+        '/[ÓÒÔÕÖ]/u'    =>   'O',
+        '/[úùûü]/u'     =>   'u',
+        '/[ÚÙÛÜ]/u'     =>   'U',
+        '/ç/'           =>   'c',
+        '/Ç/'           =>   'C',
+        '/ñ/'           =>   'n',
+        '/Ñ/'           =>   'N',
+        '/–/'           =>   '-', // UTF-8 hyphen to "normal" hyphen
+        '/[’‘‹›‚]/u'    =>   ' ', // Literally a single quote
+        '/[“”«»„]/u'    =>   ' ', // Double quote
+        '/ /'           =>   ' ', // nonbreaking space (equiv. to 0x160)
+    );
+    return preg_replace(array_keys($utf8), array_values($utf8), $text);
 }
