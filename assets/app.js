7a43a805f9d0e00d1cdd13f4fa874715deea34dc
michael <michael@cadot.info>
2021-03-18 09:45:51 +0100

    feat: symfony, bootstrap, jquery, popover, scss, webpack

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+const $ = require("jquery");
+global.$ = global.jQuery = $;
+import "bootstrap";
+
+//import "bootstrap/dist/css/bootstrap.css";
+
+import './styles/app.scss';
+$('[data-toggle="popover"]').popover();
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+import './bootstrap';
+
