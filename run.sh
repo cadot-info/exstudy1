2474c8a0972470987f0771b6a9293eca9015b2d3
michael <michael@cadot.info>
2021-03-19 01:50:33 +0100

    feat: page avancees

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker kill $(docker ps -q)
 docker rm $result
 docker run -d -p 80:8000  \
     --name $result \
-    -v $res:/app \
+    -v $res:/var/www/html \
 cadotinfo/docker-php7.4-apache-yarn-composer-symfony5
